4ab8aa500bc8f68dc31e343acf1c909b9cda4b6e
shashikdm <shashikant.123kadam@gmail.com>
2019-05-22 14:30:01 +0530

    wokring prototype

--- app.js
+++ app.js
@@ -1,10 +1,44 @@
 const request = require('request')
 
-const url = 'https://api.darksky.net/forecast/76ab85f36cc84e3b4db6c6e378d38b8c/37.8267,-122.4233?units=si&exclude=minutely,hourly,alerts,flags'
-
-request({ url: url, json: true}, (error, response) => {
-    info = response.body
-    console.log('Temperature: '+ info.currently.temperature +'C')
-    console.log('Rain chance: '+ info.currently.precipProbability*100 +'%')
-    console.log('Summary: '+info.daily.data[0].summary)
-})
+const searchString = 'ponda goa'
+const geoCode = (searchString, callback) => {
+    const mapUrl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/'+searchString+'.json?access_token=pk.eyJ1Ijoic2hhc2hpa2RtIiwiYSI6ImNqdnl4OTZ5aTBrbTUzem1qMHlmMGpqZ2UifQ.u5fb-8DvolHui_NPhATDdg'
+    request({ url: mapUrl, json: true}, (error, response) => {
+        if(error) {
+            const errMsg = 'Unable to connect to maps service'
+            console.log(errMsg)
+        } else if(response.body.error) {
+            const errMsg = response.body.error
+            console.log(errMsg)
+        } else if(response.body.features.length == 0) {
+            const errMsg = 'Unable to find Location: '+searchString
+            console.log(errMsg)
+        } else {
+            const info = response.body
+            const longitude = info.features[0].center[0]
+            const latitude = info.features[0].center[1]
+            callback(latitude, longitude)
+        }
+    })
+}
+const WeatherCode = (latitude, longitude) => {
+    console.log('Latitude: '+latitude+','+' Longitude: '+longitude)
+    const queries = '?units=si&exclude=minutely,hourly,alerts,flags'
+    const weatherUrl = 'https://api.darksky.net/forecast/76ab85f36cc84e3b4db6c6e378d38b8c/'+latitude+','+longitude+queries
+    request({ url: weatherUrl, json: true}, (error, response) => {
+        if(error) {
+            const errMsg = 'Unable to connect to weather service'
+            console.log(errMsg)
+        } else if(response.body.error) {
+            const errMsg = response.body.error
+            console.log(errMsg)
+        } else {
+            const info = response.body
+            console.log('Temperature: '+ info.currently.temperature +'C')
+            console.log('Rain chance: '+ info.currently.precipProbability*100 +'%')
+            console.log('Summary: '+info.daily.data[0].summary)
+        }
+    })
+}
+console.log(searchString)
+geoCode(searchString, WeatherCode)
