e5a037984490cd38ac76ecb63193813d97afc0b1
shashikdm <shashikant.123kadam@gmail.com>
2019-05-23 20:41:55 +0530

    minor revision

--- app.js
+++ app.js
@@ -3,21 +3,21 @@ const weatherCode = require('./utils/weathercode.js')
 const searchString = process.argv[2]
 
 if(!searchString) {
-    return console.log('Please provide a command line argument')
-}
-
-geoCode(process.argv[2], (error, {location, latitude, longitude}) => {
-    if(error) {
-        return console.log(error)
-    }
-    console.log('Location: '+location)
-    console.log('Latitude: '+latitude+','+' Longitude: '+longitude)
-    weatherCode(latitude, longitude, (error, {temperature, rainprob, summary}) => {
+    console.log('Please provide a command line argument')
+} else {
+    geoCode(process.argv[2], (error, {location, latitude, longitude}) => {
         if(error) {
             return console.log(error)
         }
-        console.log('Temperature: '+ temperature +'C')
-        console.log('Rain chance: '+ rainprob*100 +'%')
-        console.log('Summary: '+ summary)
+        console.log('Location: '+location)
+        console.log('Latitude: '+latitude+','+' Longitude: '+longitude)
+        weatherCode(latitude, longitude, (error, {temperature, rainprob, summary}) => {
+            if(error) {
+                return console.log(error)
+            }
+            console.log('Temperature: '+ temperature +'C')
+            console.log('Rain chance: '+ rainprob*100 +'%')
+            console.log('Summary: '+ summary)
+        })
     })
-})
+}
